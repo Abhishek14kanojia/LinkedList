847b2650818fbfb23e7e06974f757c596ea12ae7
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-10-03 21:04:54 +0530

    UC1

---
+++ Main.java
@@ -0,0 +1,17 @@
+package org.practice1;
+
+public class Main {
+		 Node head;
+
+	public static void main(String[] args) {
+		Service list = new Service();
+		
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		
+		list.printlist();
+
+	}
+
+}
---
+++ Service.java
@@ -0,0 +1,36 @@
+package org.practice1;
+
+public class Service {
+		Node head;
+		
+		public void add(int data) {
+			Node newNode = new Node(data);
+			if (head == null) {
+				head = newNode;
+				return;
+			}
+			Node currNode = head;
+			  while (currNode.next != null) {
+				  currNode = currNode.next;
+				  
+			  } currNode.next = newNode;
+		}
+		
+		public void addFirst(int data) {
+			Node newNode = new Node(data);
+			if (head == null) {
+				head = newNode;
+				return;
+			} newNode.next = head;
+			  head = newNode;
+		}
+		
+		public void printlist() {
+			Node currNode = head;
+			while (currNode != null) {
+				System.out.println(currNode.data);
+				currNode = currNode.next;
+			}	
+			System.out.println("Null");
+		}
+}
